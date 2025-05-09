323b6b3fca0da2c51e7176f3534c300cb88da445
Andressa Fontes <andressafc477@gmail.com>
2025-05-09 15:16:07 -0300

    first commit

---
+++ Projeto_WindowsTeste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Projeto_WindowsTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Avanade")]
+[assembly: AssemblyProduct("Projeto_WindowsTeste")]
+[assembly: AssemblyCopyright("Copyright © Avanade 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c23a2baa-4ff7-453c-8dc8-d4e971425636")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Projeto_WindowsTeste/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto_Windows; // Namespace do seu projeto Windows Forms
+
+namespace Projeto_Windows
+{
+    [TestClass]
+    public class Form1Tests
+    {
+        [TestMethod]
+        public void Soma_DeveRetornarSomaCorreta()
+        {
+            // Arrange
+            var form = new Form1();
+            int a = 5;
+            int b = 3;
+
+            // Act
+            int resultado = form.Soma(a, b);
+
+            // Assert
+            Assert.AreEqual(8, resultado);
+        }
+    }
+    // Add this method to the Form1 class in WindowsFormsApp_fw/Form1.cs
+    public partial class Form1 : Form1Tests
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
